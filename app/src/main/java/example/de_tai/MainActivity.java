74f8f3987d045233169c06ad76de4f769da9cca4
thienan137 <92162400+thienan137@users.noreply.github.com>
2023-11-17 22:13:09 +0700

    design layout

---
+++ app/src/main/java/example/de_tai/MainActivity.java
@@ -0,0 +1,16 @@
+package example.de_tai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Tường iu Lemon Diễm Hằng
+    }
+}
