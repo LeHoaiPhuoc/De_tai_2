7d611726fd96407d13a15db20b666628d6e80212
Tuo238 <billbui3002@gmail.com>
2023-11-20 16:04:57 +0700

    Add animated background + custom border

--- app/src/main/java/example/de_tai/MainActivity.java
+++ app/src/main/java/example/de_tai/MainActivity.java
@@ -1,8 +1,13 @@
 package example.de_tai;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Tường iu Lemon Diễm Hằng
+        //Set background
+        RelativeLayout relativeLayout = findViewById(R.id.main_layout);
+        Calendar calendar = Calendar.getInstance();
+        int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if(currentHour >= 5 && currentHour <= 11) {
+            AnimationDrawable animationDrawable = (AnimationDrawable) getResources().getDrawable(R.drawable.gradient_list_morning);
+            relativeLayout.setBackground(animationDrawable);
+            animationDrawable.setEnterFadeDuration(2500);
+            animationDrawable.setExitFadeDuration(5000);
+            animationDrawable.start();
+        } else if (currentHour >= 12 && currentHour <= 16) {
+            AnimationDrawable animationDrawable = (AnimationDrawable) getResources().getDrawable(R.drawable.gradient_list_afternoon);
+            relativeLayout.setBackground(animationDrawable);
+            animationDrawable.setEnterFadeDuration(2500);
+            animationDrawable.setExitFadeDuration(5000);
+            animationDrawable.start();
+        } else if(currentHour >= 17 && currentHour <= 19) {
+            AnimationDrawable animationDrawable = (AnimationDrawable) getResources().getDrawable(R.drawable.gradient_list_evening);
+            relativeLayout.setBackground(animationDrawable);
+            animationDrawable.setEnterFadeDuration(2500);
+            animationDrawable.setExitFadeDuration(5000);
+            animationDrawable.start();
+        } else if (currentHour >= 20 && currentHour <= 4) {
+            AnimationDrawable animationDrawable = (AnimationDrawable) getResources().getDrawable(R.drawable.gradient_list_night);
+            relativeLayout.setBackground(animationDrawable);
+            animationDrawable.setEnterFadeDuration(2500);
+            animationDrawable.setExitFadeDuration(5000);
+            animationDrawable.start();
+        }
+
     }
 }
