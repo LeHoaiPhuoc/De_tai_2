0d0ea64081764c9546c15cebe04aec21e9dbab41
nankiee <trannhatnam912@gmail.com>
2023-11-28 12:41:17 +0700

    GD1

---
+++ app/src/main/java/example/de_tai/GD1.java
@@ -0,0 +1,14 @@
+package example.de_tai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GD1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gd1);
+    }
+}
